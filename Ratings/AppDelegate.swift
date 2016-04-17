85ded0a76266caacda880055f471a5d840fffb33
Mike Mostachetti <mjmostachetti@gmail.com>
2016-04-16 23:00:46 -0700

    Working tutorial.

--- Ratings/AppDelegate.swift
+++ Ratings/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// delegates entrypoint for the module
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
---
+++ Ratings/SampleData.swift
@@ -0,0 +1,15 @@
+//
+//  SampleData.swift
+//  Ratings
+//
+//  Created by Michael Mostachetti on 4/16/16.
+//  Copyright © 2016 mike. All rights reserved.
+//
+
+import Foundation
+
+let playersData = [
+    Player(name: "Bill Evans", game:"Tic-tac-toe", rating: 4),
+    Player(name:"Oscar Peterson", game:"Spin the Bottle", rating: 5),
+    Player(name:"Dave Brubeck", game: "Texas Hold'em Poker", rating: 5)
+]
